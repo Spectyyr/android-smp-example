e276274cc058a721088b996e85fb5d2f5213f1d0
Zhenhao Lei <zhenhaolei@sessionm.com>
2016-08-05 11:09:46 -0400

    Bumped SDK to 2.0.1

--- app/src/main/java/com/sessionm/mmc/service/GCMListenerService.java
+++ app/src/main/java/com/sessionm/mmc/service/GCMListenerService.java
@@ -4,17 +4,12 @@
 
 package com.sessionm.mmc.service;
 
+import android.app.Notification;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Bundle;
-import android.support.v4.app.NotificationCompat;
 
-import com.sessionm.api.SessionM;
 import com.sessionm.api.message.notification.data.NotificationMessage;
 import com.sessionm.mmc.R;
 
@@ -22,47 +17,36 @@ public class GCMListenerService extends com.sessionm.api.message.notification.se
     public static final int NOTIFICATION_ID = 1;
 
     /**
-     * Uncomment this block to override sendNotification method for customized push.
+     * Change this block to override sendNotification method for customized push.
      */
-    /*@Override
-    public void sendNotification(NotificationMessage notificationMessage) {
+    @Override
+    public void sendNotification(NotificationMessage pushNotificationData) {
         NotificationManager mNotificationManager = (NotificationManager)
                 this.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        //By default and for now, open app
-        Intent LaunchIntent = getPackageManager().getLaunchIntentForPackage(getPackageName());
-        if (SessionM.getInstance().getMessageManager().shouldUseBundleExtras(this.getApplicationContext())) {
-            Bundle messageBundle = new Bundle();
-            messageBundle.putString(NotificationMessage.SESSIONM_MESSAGE_DATA_KEY, notificationMessage.getJsonString());
-            LaunchIntent.putExtras(messageBundle);
-        }
-        PendingIntent openAppIntent = PendingIntent.getActivity(this, 0, LaunchIntent, 0);
-
-        //By default, handle deep link.
-        if (!SessionM.getInstance().getMessageManager().shouldUseBundleExtras(this.getApplicationContext())) {
-            String url = notificationMessage.getActionURL();
-            if (url != null && !url.isEmpty()) {
-                openAppIntent = PendingIntent.getActivity(this, 0,
-                        new Intent(Intent.ACTION_VIEW, Uri.parse(url)), 0);
-            }
-        }
-
         Bitmap largeIcon = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
 
-        NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(this)
+        Notification.Builder mBuilder =
+                new Notification.Builder(this)
                         .setSmallIcon(getNotificationSmallIcon())
                         .setLargeIcon(largeIcon)
-                        .setContentTitle(notificationMessage.getTitle())
-                        .setContentText(notificationMessage.getBody())
-                        .setStyle(new NotificationCompat.BigTextStyle()
-                                .bigText(notificationMessage.getBody()))
-                        .setContentIntent(openAppIntent);
+                        .setAutoCancel(true)
+                        .setContentTitle(pushNotificationData.getTitle())
+                        .setContentText(pushNotificationData.getBody())
+                        .setStyle(new Notification.BigTextStyle()
+                                .bigText(pushNotificationData.getBody()))
+                        //Override pendingIntent to have customized behaviors
+                        .setContentIntent(pendingIntent);
+        //Multiple Action for future use
+        //.addAction(0, pushNotificationData.getAction(), openAppIntent);
+        //if (url != null && !url.isEmpty())
+        //    mBuilder.addAction(0, pushNotificationData.getAction1(), openBrowserIntent);
+        //mBuilder.setContentIntent(contentIntent);
         mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
     }
 
     private int getNotificationSmallIcon() {
         boolean whiteIcon = (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP);
         return whiteIcon ? R.mipmap.ic_push : R.mipmap.ic_launcher;
-    }*/
+    }
 }
