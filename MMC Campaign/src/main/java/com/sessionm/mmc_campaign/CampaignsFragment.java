7a9a3b2bdf54c3792895afe357d9a530328a20d9
Zhenhao Lei <zhenhaolei@sessionm.com>
2016-10-11 15:09:18 -0400

    Init commit. Added campaign sample app

---
+++ MMC Campaign/src/main/java/com/sessionm/mmc_campaign/CampaignsFragment.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2016 SessionM. All rights reserved.
+ */
+
+package com.sessionm.mmc_campaign;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.sessionm.api.SessionM;
+import com.sessionm.api.SessionMError;
+import com.sessionm.api.campaign.CampaignsListener;
+import com.sessionm.api.campaign.CampaignsManager;
+import com.sessionm.api.message.data.Message;
+import com.sessionm.api.message.feed.data.FeedMessage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Fragment of SessionM List of Message
+ */
+public class CampaignsFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+    private static final String TAG = "FeedListActivity";
+
+    private SwipeRefreshLayout _swipeRefreshLayout;
+    private CampaignsRecAdapter _campaignsRecAdapter;
+    //private List of SessionM Message
+    private List<FeedMessage> _messages;
+    private RecyclerView _recyclerView;
+
+    OnDeepLinkTappedListener onDeepLinkTappedListener;
+
+    private CampaignsManager _campaignsManager = SessionM.getInstance().getCampaignsManager();
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_campaigns, container, false);
+        ViewCompat.setElevation(rootView, 50);
+
+        _swipeRefreshLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.swipe_refresh_layout);
+        _swipeRefreshLayout.setOnRefreshListener(this);
+
+        _campaignsManager.setListener(_campaignsListener);
+        _messages = new ArrayList<>(_campaignsManager.getFeedMessages());
+
+        _recyclerView = (RecyclerView) rootView.findViewById(R.id.message_feed_list);
+        _recyclerView.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(getContext());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        _recyclerView.setLayoutManager(llm);
+        _campaignsRecAdapter = new CampaignsRecAdapter(this, _messages);
+        _recyclerView.setAdapter(_campaignsRecAdapter);
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        _campaignsManager.fetchFeedMessages();
+    }
+
+    CampaignsListener _campaignsListener = new CampaignsListener() {
+        @Override
+        public void onFeedMessagesFetched(List<FeedMessage> list) {
+            _swipeRefreshLayout.setRefreshing(false);
+            if (_messages == null) {
+                _messages = new ArrayList<>();
+            } else {
+                _messages.clear();
+            }
+            _messages.addAll(list);
+            _campaignsRecAdapter.notifyDataSetChanged();
+        }
+
+        @Override
+        public void onFailure(SessionMError error) {
+            _swipeRefreshLayout.setRefreshing(false);
+            Toast.makeText(getActivity(), "Failed: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        _campaignsManager.setListener(_campaignsListener);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onRefresh() {
+        _campaignsManager.fetchFeedMessages();
+    }
+
+    public void onItemTapped(Message.MessageActionType actionType, String actionURL) {
+        onDeepLinkTappedListener.onDeepLinkTapped(actionType, actionURL);
+    }
+
+    //On deep link listener to talk up to activity
+    public interface OnDeepLinkTappedListener {
+        void onDeepLinkTapped(Message.MessageActionType actionType, String actionURL);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try {
+            onDeepLinkTappedListener = (OnDeepLinkTappedListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+                    + " must implement OnDeepLinkTappedListener");
+        }
+    }
+}
---
+++ MMC Campaign/src/main/java/com/sessionm/mmc_campaign/CampaignsRecAdapter.java
@@ -0,0 +1,173 @@
+/*
+ * Copyright (c) 2016 SessionM. All rights reserved.
+ */
+
+package com.sessionm.mmc_campaign;
+
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.VideoView;
+
+import com.sessionm.api.SessionM;
+import com.sessionm.api.message.data.Message;
+import com.sessionm.api.message.feed.data.FeedMessage;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+//Adapter class to draw the Promotions Message List and handle Feed Message events
+public class CampaignsRecAdapter extends RecyclerView.Adapter<CampaignsRecAdapter.CampaignsViewHolder> {
+
+    private CampaignsFragment _fragment;
+    private List<FeedMessage> _messages;
+
+    public CampaignsRecAdapter(CampaignsFragment fragment, List<FeedMessage> messages) {
+        _fragment = fragment;
+        _messages = messages;
+    }
+
+    @Override
+    public CampaignsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        final View itemView = LayoutInflater.
+                from(parent.getContext()).
+                inflate(R.layout.feed_item_campaign, parent, false);
+
+        return new CampaignsViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(CampaignsViewHolder holder, int position) {
+        final FeedMessage item = _messages.get(position);
+
+        //Returns the message header
+        holder.headerTextView.setText(item.getHeader());
+
+        //Returns the message sub header
+        holder.subHeaderTextView.setText(item.getSubHeader());
+
+        //Returns the message period
+        holder.periodTextView.setText(item.getStartTime() + " - " + item.getEndTime());
+
+        //There is no need to draw the description if it was not set
+        if (!TextUtils.isEmpty(item.getDescription())) {
+            //Returns the Message description, String
+            holder.descriptionTextView.setText(item.getDescription());
+            holder.descriptionTextView.setVisibility(View.VISIBLE);
+        } else {
+            holder.descriptionTextView.setVisibility(View.GONE);
+        }
+
+        //TODO: set value, might be points
+        int points = item.getPoints();
+        if (points == 0)
+            holder.valueTextView.setVisibility(View.GONE);
+        else
+            holder.valueTextView.setText(points + " pts");
+
+        //Any customized value in data field
+        /*JSONObject data = item.getData();
+        if (data != null) {
+            String value = data.optString("value");
+            valueTextView.setText(value);
+        }*/
+
+        //There is no need to draw the image if there is not icon URL
+        if (item.getIconURL() != null && !item.getIconURL().equals("null")) {
+            //Returns the Message image URL, String
+            Picasso.with(_fragment.getActivity()).load(item.getIconURL()).into(holder.iconImageView);
+            holder.iconImageView.setVisibility(View.VISIBLE);
+        } else {
+            holder.iconImageView.setVisibility(View.GONE);
+        }
+
+        //There is no need to draw the image if there is not image URL
+        String imageURL = item.getImageURL();
+        if (imageURL != null && !imageURL.equals("null")) {
+            if (imageURL.endsWith("mp4")) {
+                final Uri videoUri = Uri.parse(imageURL);
+                holder.feedImageView.setVisibility(View.GONE);
+                holder.videoView.setVisibility(View.VISIBLE);
+                holder.videoView.setVideoURI(videoUri);
+                holder.videoView.start();
+                holder.videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                    @Override
+                    public void onPrepared(MediaPlayer mp) {
+                    }
+                });
+                holder.videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                    }
+                });
+            } else {
+                //Returns the Message image URL, String
+                Picasso.with(_fragment.getActivity()).load(item.getImageURL()).into(holder.feedImageView);
+                holder.feedImageView.setVisibility(View.VISIBLE);
+                holder.videoView.setVisibility(View.GONE);
+            }
+        } else {
+            holder.feedImageView.setVisibility(View.GONE);
+            holder.videoView.setVisibility(View.GONE);
+        }
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                item.notifyTapped();
+                showDetails(item);
+            }
+        });
+
+        item.notifySeen();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getItemCount() {
+        return _messages.size();
+    }
+
+    //TODO Needs to handle more events
+    private void showDetails(FeedMessage data) {
+        Message.MessageActionType actionType = data.getActionType();
+        if (actionType.equals(Message.MessageActionType.FULL_SCREEN)) {
+            SessionM.getInstance().presentActivity(SessionM.ActivityType.PORTAL, data.getActionURL());
+        } else {
+            _fragment.onItemTapped(actionType, data.getActionURL());
+        }
+    }
+
+    public static class CampaignsViewHolder extends RecyclerView.ViewHolder {
+        ImageView iconImageView;
+        TextView headerTextView;
+        TextView subHeaderTextView;
+        TextView periodTextView;
+        TextView descriptionTextView;
+        TextView valueTextView;
+        ImageView feedImageView;
+        VideoView videoView;
+
+        public CampaignsViewHolder(View v) {
+            super(v);
+            iconImageView = (ImageView) v.findViewById(R.id.promotion_icon_image);
+            headerTextView = (TextView) v.findViewById(R.id.promotion_header_text);
+            subHeaderTextView = (TextView) v.findViewById(R.id.promotion_subheader_text);
+            periodTextView = (TextView) v.findViewById(R.id.promotion_period_text);
+            descriptionTextView = (TextView) v.findViewById(R.id.promotion_detail_text);
+            valueTextView = (TextView) v.findViewById(R.id.promotion_value_text);
+            feedImageView = (ImageView) v.findViewById(R.id.promotion_main_image);
+            videoView = (VideoView) v.findViewById(R.id.promotion_main_video);
+        }
+    }
+}
