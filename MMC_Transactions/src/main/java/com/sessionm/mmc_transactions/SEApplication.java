8e0b542ec36647a84ccbe9e8ea028eea91b86c44
Zhenhao Lei <zhenhaolei@sessionm.com>
2016-10-12 14:53:23 -0400

    Added Transactions app

---
+++ MMC_Transactions/src/main/java/com/sessionm/mmc_transactions/SEApplication.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2016 SessionM. All rights reserved.
+ */
+
+package com.sessionm.mmc_transactions;
+
+import android.app.Application;
+
+import com.sessionm.api.SessionM;
+import com.sessionm.api.SessionMActivityLifecycleCallbacks;
+
+public class SEApplication extends Application{
+    private static final String TAG = "AppController";
+    private final SessionMActivityLifecycleCallbacks _mCallbacks = new SessionMActivityLifecycleCallbacks();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //Creates SessionM activity lifecycle callbacks to handle activities lifecycle
+        registerActivityLifecycleCallbacks(_mCallbacks);
+        final SessionM sessionM = SessionM.getInstance();
+        sessionM.setApplicationContext(this);
+    }
+}
