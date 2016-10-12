0998ca3a1e5a41221f70311d28ec332445b56290
Zhenhao Lei <zhenhaolei@sessionm.com>
2016-10-12 15:55:22 -0400

    Added user to campaigns module

--- MMC_Campaigns/src/main/java/com/sessionm/mmc_campaigns/MainActivity.java
+++ MMC_Campaigns/src/main/java/com/sessionm/mmc_campaigns/MainActivity.java
@@ -2,15 +2,72 @@ package com.sessionm.mmc_campaigns;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
+import com.sessionm.api.AchievementData;
+import com.sessionm.api.SessionListener;
+import com.sessionm.api.SessionM;
+import com.sessionm.api.User;
 import com.sessionm.api.message.data.Message;
+import com.sessionm.api.message.notification.data.NotificationMessage;
 
-public class MainActivity extends AppCompatActivity implements CampaignsFragment.OnDeepLinkTappedListener {
+public class MainActivity extends AppCompatActivity implements SessionListener, CampaignsFragment.OnDeepLinkTappedListener  {
+
+    private static final String SAMPLE_USER_TOKEN = "v2--uptXiU8SpBL-lAMK2Rvk0-qwFe01i9JV4nq__RWmsA=-B3Csmpxi8IQmmv59LexE6L7hoN3tscIlbA3Yjoab8Xu9pFCAHgJ-y4OXuPA_Vc-n8w==";
+
+    private TextView userBalanceTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar actionBar = (Toolbar) findViewById(R.id.custom_action_bar);
+        setSupportActionBar(actionBar);
+
+        userBalanceTextView = (TextView) findViewById(R.id.user_balance_textview);
+        final SessionM sessionM = SessionM.getInstance();
+        userBalanceTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!sessionM.getUser().isRegistered())
+                    //sessionM.authenticateWithToken("sessionm", SAMPLE_USER_TOKEN);
+                    sessionM.logInUserWithEmail("mmcsampleuser@sessionm.com", "sessionm1");
+                else
+                    sessionM.logOutUser();
+            }
+        });
+    }
+
+    @Override
+    public void onSessionStateChanged(SessionM sessionM, SessionM.State state) {
+
+    }
+
+    @Override
+    public void onSessionFailed(SessionM sessionM, int i) {
+
+    }
+
+    @Override
+    public void onUserUpdated(SessionM sessionM, User user) {
+        if (user.isRegistered())
+            userBalanceTextView.setText(user.getPointBalance() + "pts");
+        else
+            userBalanceTextView.setText(getString(R.string.click_here_to_log_in_user));
+        sessionM.getCampaignsManager().fetchFeedMessages();
+    }
+
+    @Override
+    public void onUnclaimedAchievement(SessionM sessionM, AchievementData achievementData) {
+
+    }
+
+    @Override
+    public void onNotificationMessage(SessionM sessionM, NotificationMessage notificationMessage) {
+
     }
 
     @Override
