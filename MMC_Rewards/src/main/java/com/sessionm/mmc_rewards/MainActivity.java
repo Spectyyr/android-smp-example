48ddb5592b66753aafc16862915cbd0e95b8f466
Zhenhao Lei <zhenhaolei@sessionm.com>
2016-10-14 16:44:16 -0400

    Added orders in Rewards Example

--- MMC_Rewards/src/main/java/com/sessionm/mmc_rewards/MainActivity.java
+++ MMC_Rewards/src/main/java/com/sessionm/mmc_rewards/MainActivity.java
@@ -1,9 +1,13 @@
 package com.sessionm.mmc_rewards;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
 
 import com.sessionm.api.AchievementData;
@@ -17,6 +21,7 @@ public class MainActivity extends AppCompatActivity implements SessionListener {
     private static final String SAMPLE_USER_TOKEN = "v2--uptXiU8SpBL-lAMK2Rvk0-qwFe0-1i9JV4nq__RWmsA=--B3Csmpxi8IQmmv59LexE6L7hoN3tscIlbA3Yjoab8Xu9pFCAHgJ-y4OXuPA_Vc-n8w==";
 
     private TextView userBalanceTextView;
+    private TextView ordersTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +31,8 @@ public class MainActivity extends AppCompatActivity implements SessionListener {
         Toolbar actionBar = (Toolbar) findViewById(R.id.custom_action_bar);
         setSupportActionBar(actionBar);
 
-        userBalanceTextView = (TextView) findViewById(R.id.user_balance_textview);
         final SessionM sessionM = SessionM.getInstance();
+        userBalanceTextView = (TextView) findViewById(R.id.user_balance_textview);
         userBalanceTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,6 +40,14 @@ public class MainActivity extends AppCompatActivity implements SessionListener {
                     sessionM.authenticateWithToken("auth_token", SAMPLE_USER_TOKEN);
                 else
                     sessionM.logOutUser();
+            }
+        });
+
+        ordersTextView = (TextView) findViewById(R.id.orders_textview);
+        ordersTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, OrdersActivity.class));
             }
         });
     }
---
+++ MMC_Rewards/src/main/java/com/sessionm/mmc_rewards/OrdersFragment.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2016 SessionM. All rights reserved.
+ */
+
+package com.sessionm.mmc_rewards;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.sessionm.api.SessionM;
+import com.sessionm.api.SessionMError;
+import com.sessionm.api.reward.RewardsListener;
+import com.sessionm.api.reward.RewardsManager;
+import com.sessionm.api.reward.data.offer.Offer;
+import com.sessionm.api.reward.data.order.Order;
+import com.sessionm.api.reward.data.skill.SkillChallenge;
+import com.sessionm.api.reward.data.skill.SkillQuestion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Fragment of SessionM Rewards
+public class OrdersFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+
+    private SwipeRefreshLayout _swipeRefreshLayout;
+    private OrdersFeedListAdapter _listAdapter;
+    private List<Order> _orders;
+    private RecyclerView _recyclerView;
+
+    private RewardsManager _rewardsManager = SessionM.getInstance().getRewardsManager();
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_orders, container, false);
+        ViewCompat.setElevation(rootView, 50);
+
+        _swipeRefreshLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.swipe_refresh_layout);
+        _swipeRefreshLayout.setOnRefreshListener(this);
+
+        _orders = new ArrayList<>(_rewardsManager.getOrders());
+
+        _recyclerView = (RecyclerView) rootView.findViewById(R.id.orders_feed_list);
+        _recyclerView.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(getContext());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        _recyclerView.setLayoutManager(llm);
+        _listAdapter = new OrdersFeedListAdapter(this, _orders);
+        _recyclerView.setAdapter(_listAdapter);
+
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        _rewardsManager.fetchOrders();
+    }
+
+    RewardsListener _rewardsListener = new RewardsListener() {
+        @Override
+        public void onOffersFetched(List<Offer> list) {
+
+        }
+
+        @Override
+        public void onOrderPlaced(Order order) {
+
+        }
+
+        @Override
+        public void onOrdersFetched(List<Order> orders) {
+            _swipeRefreshLayout.setRefreshing(false);
+            if (OrdersFragment.this._orders == null) {
+                OrdersFragment.this._orders = new ArrayList<>();
+            } else {
+                OrdersFragment.this._orders.clear();
+            }
+            OrdersFragment.this._orders.addAll(orders);
+            _listAdapter.notifyDataSetChanged();
+        }
+
+        @Override
+        public void onSkillQuestionFetched(SkillQuestion skillQuestion) {
+
+        }
+
+        @Override
+        public void onSkillQuestionAnswered(SkillChallenge skillChallenge) {
+
+        }
+
+        @Override
+        public void onFailure(SessionMError error) {
+            _swipeRefreshLayout.setRefreshing(false);
+            Toast.makeText(getActivity(), "Failed: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+    };
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        _rewardsManager.setListener(_rewardsListener);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onRefresh() {
+        _rewardsManager.fetchOrders();
+    }
+}
