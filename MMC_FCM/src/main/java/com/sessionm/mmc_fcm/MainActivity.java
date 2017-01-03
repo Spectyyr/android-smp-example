90b18d488a3eb0dcfe07b3ebba5a602966f7f8b4
Zhenhao Lei <zhenhaolei@sessionm.com>
2017-01-03 10:45:58 -0500

    Bumped FCM example app to use android SDK 2.1.3 with new Message Listener logic

--- MMC_FCM/src/main/java/com/sessionm/mmc_fcm/MainActivity.java
+++ MMC_FCM/src/main/java/com/sessionm/mmc_fcm/MainActivity.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.sessionm.api.AchievementData;
 import com.sessionm.api.SessionListener;
 import com.sessionm.api.SessionM;
+import com.sessionm.api.SessionMError;
 import com.sessionm.api.User;
+import com.sessionm.api.message.MessagesListener;
+import com.sessionm.api.message.data.Message;
 import com.sessionm.api.message.notification.data.NotificationMessage;
 
 public class MainActivity extends AppCompatActivity implements SessionListener {
@@ -17,17 +23,22 @@ public class MainActivity extends AppCompatActivity implements SessionListener {
     private static final String SAMPLE_USER_TOKEN = "v2--Sd2T8UBqlCGQovVPnsUs4eqwFe0-1i9JV4nq__RWmsA=--dWM8r8RggUJCToOaiiT6NXmiOipkovvD9HueM_jZECStExtGFkZzVmCUhkdDJe5NQw==";
 
     private TextView userBalanceTextView;
+    private ToggleButton useBundleExtrasButton;
+    private NotificationMessage pushMessage;
+    private SessionM sessionM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sessionM = SessionM.getInstance();
+
+
         Toolbar actionBar = (Toolbar) findViewById(R.id.custom_action_bar);
         setSupportActionBar(actionBar);
 
         userBalanceTextView = (TextView) findViewById(R.id.user_balance_textview);
-        final SessionM sessionM = SessionM.getInstance();
         userBalanceTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,6 +48,22 @@ public class MainActivity extends AppCompatActivity implements SessionListener {
                     sessionM.logOutUser();
             }
         });
+
+        useBundleExtrasButton = (ToggleButton) findViewById(R.id.use_bundle_extras_toggle);
+        useBundleExtrasButton.setChecked(sessionM.getMessageManager().isPushNotificationEnabled());
+        useBundleExtrasButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                sessionM.getMessageManager().setUseBundleExtrasEnabled(isChecked);
+            }
+        });
+
+        //Only needed when use bundle extras enabled.
+        if (sessionM.getMessageManager().isUseBundleExtrasEnabled()) {
+            Bundle extras = getIntent().getExtras();
+            pushMessage = sessionM.getMessageManager().getPendingNotification(extras);
+            setUpPushMessaging();
+        }
     }
 
     @Override
@@ -62,12 +89,48 @@ public class MainActivity extends AppCompatActivity implements SessionListener {
 
     }
 
-    @Override
-    public void onNotificationMessage(SessionM sessionM, NotificationMessage notificationMessage) {
-
-    }
-
     public void TriggerOpenAdPush(View view) {
         SessionM.getInstance().logAction("push_notification_open_ad");
+    }
+
+    //Only needed when use bundle extras enabled.
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (pushMessage != null) {
+            sessionM.getMessageManager().executePendingNotificationFromPush(pushMessage);
+        }
+    }
+
+    //Only needed when use bundle extras enabled.
+    private void setUpPushMessaging() {
+        SessionM.getInstance().getMessageManager().setListener(new MessagesListener() {
+            @Override
+            public void onNotificationMessage(NotificationMessage notificationMessage) {
+                handleMessageAction(notificationMessage);
+                pushMessage = null;
+                Toast.makeText(MainActivity.this, "New push notification received!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(SessionMError sessionMError) {
+
+            }
+        });
+    }
+
+    //Only needed when use bundle extras enabled.
+    private void handleMessageAction(NotificationMessage message) {
+        // Optionally check message type
+        if (message == null)
+            return;
+        if (message.getActionType() == Message.MessageActionType.EXTERNAL_LINK) {
+            // Launch URL in Mobile Browser
+            // Provide sample code
+        } else if (message.getActionType() == Message.MessageActionType.DEEP_LINK) {
+            // handle navigation to deep line
+        } else {
+            // Log Error
+        }
     }
 }
