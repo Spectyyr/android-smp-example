3575967e03d2b6232403a95a417da1d810234cc4
Zhenhao Lei <zhenhaolei@sessionm.com>
2017-02-01 16:32:18 -0500

    Added push notification support for geofence module

--- SMP_Geofence/src/main/java/com/sessionm/smp_geofence/SEApplication.java
+++ SMP_Geofence/src/main/java/com/sessionm/smp_geofence/SEApplication.java
@@ -22,6 +22,13 @@ public class SEApplication extends Application{
         registerActivityLifecycleCallbacks(_mCallbacks);
         final SessionM sessionM = SessionM.getInstance();
         sessionM.setApplicationContext(this);
-        //sessionM.setServerType(SessionM.SERVER_TYPE_CUSTOM, "https://api.tour-sessionm.com");
+        sessionM.setServerType(SessionM.SERVER_TYPE_CUSTOM, "https://api.tour-sessionm.com");
+
+        //Enable push notification. Add this block to accept follow up push notification triggered by geofence events.
+
+        //Sets the Google Cloud Messaging Sender ID, to register the app to be able to receive push notifications
+        sessionM.getMessageManager().setGCMSenderID(getString(R.string.gcm_sender_id));
+        //Enables SessionM to receive push notifications, generates and sends a token to the server so the device can receive push notifications
+        sessionM.getMessageManager().setPushNotificationEnabled(true);
     }
 }
