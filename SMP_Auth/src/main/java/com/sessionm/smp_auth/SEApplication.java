937cc9ebc5f63bac9a15129e9cdcfe81e5be5d39
Zhenhao Lei <zhenhaolei@sessionm.com>
2017-05-01 09:51:44 -0400

    Demo server for Auth

--- SMP_Auth/src/main/java/com/sessionm/smp_auth/SEApplication.java
+++ SMP_Auth/src/main/java/com/sessionm/smp_auth/SEApplication.java
@@ -18,8 +18,12 @@ public class SEApplication extends Application{
         registerActivityLifecycleCallbacks(new SessionMActivityLifecycleCallbacks());
         final SessionM sessionM = SessionM.getInstance();
         sessionM.setApplicationContext(this);
-        sessionM.setServerType(SessionM.SERVER_TYPE_CUSTOM, "https://api.tour-sessionm.com");
-        sessionM.setAppKey("c4d9427e894e3b0ddf67bd08280b11e051f66c82");
+//        sessionM.setServerType(SessionM.SERVER_TYPE_CUSTOM, "https://api.tour-sessionm.com");
+//        sessionM.setAppKey("c4d9427e894e3b0ddf67bd08280b11e051f66c82");
+
+        sessionM.setServerType(SessionM.SERVER_TYPE_CUSTOM, "https://api-demo.stg-sessionm.com");
+        sessionM.setAppKey("04e54a2689ec2d96e2cd5394f377671f80166165");
+
 
 
         //TODO: support native facebook SDK
