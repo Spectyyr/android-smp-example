de14c890805a75db427f1ddbb97fa86f1e594c73
Zhenhao Lei <zhenhaolei@sessionm.com>
2017-05-10 10:53:36 -0400

    Started using sessionm version of AppAuth

--- SMP_Auth/src/main/java/com/sessionm/smp_auth/SEApplication.java
+++ SMP_Auth/src/main/java/com/sessionm/smp_auth/SEApplication.java
@@ -9,7 +9,7 @@ import android.app.Application;
 import com.sessionm.api.SessionM;
 import com.sessionm.api.SessionMActivityLifecycleCallbacks;
 
-public class SEApplication extends Application{
+public class SEApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
@@ -18,12 +18,13 @@ public class SEApplication extends Application{
         registerActivityLifecycleCallbacks(new SessionMActivityLifecycleCallbacks());
         final SessionM sessionM = SessionM.getInstance();
         sessionM.setApplicationContext(this);
+        //        sessionM.setServerType(SessionM.SERVER_TYPE_CUSTOM, "https://api-miamiheat.stg-sessionm.com");
+//        sessionM.setAppKey("848a5aa43f9bc95c7e97e3701264426ccb4c7c7b");
 //        sessionM.setServerType(SessionM.SERVER_TYPE_CUSTOM, "https://api.tour-sessionm.com");
 //        sessionM.setAppKey("c4d9427e894e3b0ddf67bd08280b11e051f66c82");
 
         sessionM.setServerType(SessionM.SERVER_TYPE_CUSTOM, "https://api-demo.stg-sessionm.com");
         sessionM.setAppKey("04e54a2689ec2d96e2cd5394f377671f80166165");
-
 
 
         //TODO: support native facebook SDK
