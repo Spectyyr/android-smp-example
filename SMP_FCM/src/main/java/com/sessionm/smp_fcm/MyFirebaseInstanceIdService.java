0e2e2c0bb4eb94959c2003bd5bab135e08d5c968
Zhenhao Lei <zhenhaolei@sessionm.com>
2017-08-21 10:02:34 -0400

    Updated to use MessagesManager instance

--- SMP_FCM/src/main/java/com/sessionm/smp_fcm/MyFirebaseInstanceIdService.java
+++ SMP_FCM/src/main/java/com/sessionm/smp_fcm/MyFirebaseInstanceIdService.java
@@ -6,7 +6,7 @@ package com.sessionm.smp_fcm;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
-import com.sessionm.api.SessionM;
+import com.sessionm.api.message.MessagesManager;
 
 public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
     @Override
@@ -17,7 +17,6 @@ public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
         // Instance ID token to your app server.
-        SessionM.getInstance().getMessageManager().setPushNotificationRegistrationID(refreshedToken);
+        MessagesManager.getInstance().setPushNotificationRegistrationID(refreshedToken);
     }
-
 }
