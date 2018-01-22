77e1e2e9be3e9789f54a257d6ae8c9e63a622bbd
Zhenhao Lei <zhenhaolei@sessionm.com>
2018-01-22 16:43:17 -0500

    Added silentStart method

--- SMP_Rewards/src/main/java/com/sessionm/smp_rewards/SEApplication.java
+++ SMP_Rewards/src/main/java/com/sessionm/smp_rewards/SEApplication.java
@@ -7,14 +7,12 @@ package com.sessionm.smp_rewards;
 import android.app.Application;
 
 import com.sessionm.api.SessionM;
-import com.sessionm.api.SessionMActivityLifecycleCallbacks;
 
 public class SEApplication extends Application{
 
     @Override
     public void onCreate() {
         super.onCreate();
-        this.registerActivityLifecycleCallbacks(new SessionMActivityLifecycleCallbacks());
-        SessionM.getInstance().setApplicationContext(getApplicationContext());
+        SessionM.getInstance().silentStart(this);
     }
 }
